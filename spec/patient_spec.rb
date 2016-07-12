a6211fdc99ab29d9424e2980928ffe6bbb891c83
Julia Dickey and Lauren Posey <julia@>
2016-07-12 11:17:38 -0700

    Pass spec: is the same patient if it has the same name and birthday

--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -2,8 +2,24 @@ require('spec_helper')
 
 describe(Patient) do
   describe('.all') do
-    it("is empty at first") do
+    it("shows the all array as empty at first") do
       expect(Patient.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a patient to the database') do
+      test_patient = Patient.new({:name => "Sam", :birthday => '1999-04-12', :doctor_id => 1})
+      test_patient.save()
+      expect(Patient.all()).to(eq([test_patient]))
+    end
+  end
+
+  describe(':==') do
+    it("is the same patient if it has the same name and birthday") do
+      patient1 = Patient.new({:name => "Sam", :birthday => "1987-02-15", :doctor_id => 1})
+      patient2 = Patient.new({:name => "Sam", :birthday => "1987-02-15", :doctor_id => 1})
+      expect(patient1).to(eq(patient2))
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,10 @@ require('pg')
 
 DB = PG.connect({:dbname => "doctors_office_test"})
 
+
 RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM doctor *;")
+    DB.exec("DELETE FROM patient *;")
   end
 end
